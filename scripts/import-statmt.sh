ba6d301b195563ffba9cc4f0d58dd279460dca5b
evgeny pavlov <epavlov@mozilla.com>
2022-02-10 11:29:10 -0800

    Add statmt importing script

---
+++ scripts/import-statmt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pair=$1
+
+mkdir -P models/prod/$pair
+
+wget -nc -P models/prod/$pair/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/vocab.$pair.spm
+wget -nc -P models/prod/$pair/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/lex.s2t.bin
+wget -nc -P models/prod/$pair/ http://data.statmt.org/bergamot/models/$pair/$pair.student.tiny11/model.intgemm.alphas.bin
+
+# follow our naming convention
+mv models/prod/$pair/lex.s2t.bin models/prod/$pair/lex.50.50.$pair.s2t.bin
+mv models/prod/$pair/model.intgemm.alphas.bin models/prod/$pair/model.$pair.intgemm.alphas.bin
+
+gzip models/prod/$pair/*
