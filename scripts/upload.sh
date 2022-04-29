c313f3ad7edc544476661560944b1f774913fe01
Evgeny Pavlov <epavlov@mozilla.com>
2022-04-29 09:17:08 -0700

    Fix upload script

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -7,4 +7,4 @@ echo ${CIRCLE_TAG} > latest.txt
 gsutil -m cp -rZn models/prod/* gs://bergamot-models-sandbox/${CIRCLE_TAG}/
 gsutil -m cp -rZn models/dev/* gs://bergamot-models-sandbox/${CIRCLE_TAG}/
 gsutil -m cp -n registry.json gs://bergamot-models-sandbox/${CIRCLE_TAG}/
-gsutil -m cp -n latest.txt gs://bergamot-models-sandbox/
+gsutil -m cp latest.txt gs://bergamot-models-sandbox/
