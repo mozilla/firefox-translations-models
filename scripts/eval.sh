35386268e3c708f2db1622521c3fc6164f657f76
Andre Natal <andrenatal@users.noreply.github.com>
2023-04-13 18:07:35 -0700

    Update eval.sh to support comet

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -5,9 +5,11 @@ set -e
 python3 eval/evaluate.py \
   --translators=bergamot,microsoft,google \
   --pairs=all --skip-existing \
-  --results-dir=/models/evaluation/dev --models-dir=/models/models/dev
+  --results-dir=/models/evaluation/dev --models-dir=/models/models/dev \
+  --gpus=1 --evaluation-engine=comet,bleu
 
 python3 eval/evaluate.py \
   --translators=bergamot,microsoft,google \
   --pairs=all --skip-existing \
-  --results-dir=/models/evaluation/prod --models-dir=/models/models/prod
+  --results-dir=/models/evaluation/prod --models-dir=/models/models/prod \
+  --gpus=1 --evaluation-engine=comet,bleu
