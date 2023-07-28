2f0b5abdd3cdc60f08304cab01d6f12fe6c530cc
evgeny pavlov <epavlov@mozilla.com>
2023-07-28 13:42:20 -0700

    Add new models from statmt

--- scripts/import-statmt.sh
+++ scripts/import-statmt.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-pair=$1
+# example:
+# bash scripts/import-statmt.sh isen isen isen.student.tiny11 dev
 
-mkdir -p models/prod/$pair
+pair=$1
+vocab_pair=$2
+model=$3
+folder=$4
+
+
+mkdir -p models/$folder/$pair
+
 
 # directory structure is not consistent on statmt
-wget -nc -P models/prod/$pair/ http://data.statmt.org/bergamot/models/fren/$pair/tiny11/lex.s2t.bin
-wget -nc -P models/prod/$pair/ http://data.statmt.org/bergamot/models/fren/$pair/tiny11/model.intgemm.alphas.bin
-wget -nc -P models/prod/$pair/ http://data.statmt.org/bergamot/models/fren/$pair/tiny11/vocab.fren.spm
+wget -nc -P models/$folder/$pair/ http://data.statmt.org/bergamot/models/$pair/$model/lex.s2t.bin
+wget -nc -P models/$folder/$pair/ http://data.statmt.org/bergamot/models/$pair/$model/model.intgemm.alphas.bin
+wget -nc -P models/$folder/$pair/ http://data.statmt.org/bergamot/models/$pair/$model/vocab.$vocab_pair.spm
 
 # follow our naming convention
-mv models/prod/$pair/lex.s2t.bin models/prod/$pair/lex.50.50.$pair.s2t.bin
-mv models/prod/$pair/model.intgemm.alphas.bin models/prod/$pair/model.$pair.intgemm.alphas.bin
+mv models/$folder/$pair/lex.s2t.bin models/$folder/$pair/lex.50.50.$pair.s2t.bin
+mv models/$folder/$pair/model.intgemm.alphas.bin models/$folder/$pair/model.$pair.intgemm.alphas.bin
 
-gzip models/prod/$pair/*
+gzip models/$folder/$pair/*
