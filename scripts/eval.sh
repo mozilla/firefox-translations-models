108578c6e0db0dd2dc454163a946ebd3944aacc0
evgeny pavlov <epavlov@mozilla.com>
2023-10-27 11:29:24 -0700

    Enable comet

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -6,10 +6,10 @@ python3 eval/evaluate.py \
   --translators=bergamot,microsoft,google,argos,nllb,opusmt \
   --pairs=all --skip-existing \
   --results-dir=/models/evaluation/dev --models-dir=/models/models/dev \
-  --gpus=1 --evaluation-engine=bleu
+  --gpus=1 --evaluation-engine=comet,bleu
 
 python3 eval/evaluate.py \
   --translators=bergamot,microsoft,google,argos,nllb,opusmt \
   --pairs=all --skip-existing \
   --results-dir=/models/evaluation/prod --models-dir=/models/models/prod \
-  --gpus=1 --evaluation-engine=bleu
+  --gpus=1 --evaluation-engine=comet,bleu
