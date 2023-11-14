416add207fec6ef0c7c05eeed4cdba7c6ddf5775
Erik Nordin <enordin@mozilla.com>
2023-11-13 20:09:31 -0600

    Add Error/Help print support
    Adds a module to help with printing formatted
    error and help messages.

---
+++ remote-settings/format.py
@@ -0,0 +1,19 @@
+import sys
+
+from termcolor import colored
+
+ERROR = colored("\nError:", "red")
+HELP = colored("\nHelp:", "green")
+
+
+def print_error(*args, **kwargs):
+    """Prints a message to stderr with the prefix 'Error:'"""
+    print(ERROR, end=" ", file=sys.stderr)
+    print(*args, file=sys.stderr, **kwargs)
+
+
+def print_help(*args, **kwargs):
+    """Prints a message with the prefix 'Help:'"""
+    print(HELP, end=" ")
+    print(*args, **kwargs)
+
