8727af9f5d6116d0f661e51edea6e280ee00fbd8
Erik Nordin <enordin@mozilla.com>
2024-01-30 12:14:12 -0600

    Add help message for no files found

--- remote_settings/subcommands/create.py
+++ remote_settings/subcommands/create.py
@@ -3,7 +3,7 @@ import argparse, os
 from packaging import version
 
 from remote_settings.client import RemoteSettingsClient
-from remote_settings.format import print_info
+from remote_settings.format import print_info, print_error, print_help
 
 
 def attach_create_subcommand(subparsers):
@@ -108,6 +108,12 @@ def do_create(args):
     print_info(args)
     print_info(args, f"User: {client.authenticated_user()}")
     print_info(args, f"Server: {client.server_url()}")
+
+    if client.record_count() == 0:
+        print_error("No records found.")
+        print_help("You may need to unzip the archives in the desired directory.")
+        exit(1)
+
     print_info(args)
 
     for i in range(client.record_count()):
@@ -117,5 +123,7 @@ def do_create(args):
             print_info(args, "Creating record...")
             client.create_new_record(i)
             print_info(args, f"{client.attachment_name(i)} created")
+
+        print_info(args)
 
     print_info(args)
