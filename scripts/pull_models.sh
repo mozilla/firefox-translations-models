3bdb99340eee9eb0971386a6b5a7ec49366c2c8f
evgeny pavlov <epavlov@mozilla.com>
2024-04-01 15:27:53 -0700

    Update the model downloading script to use Taskcluster

--- scripts/pull_models.sh
+++ scripts/pull_models.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+#
+# Downloads exported models from Taskcluster artifacts
+#
+# Example:  SRC=lt TRG=en TASK_ID=SjPZGW9CRYeb9PQr68jCUw bash pull_models.sh
+#
 
-# Downloads models from snakepit cluster
+[[ -z "${SRC}" ]] && echo "SRC is empty"
+[[ -z "${TRG}" ]] && echo "TRG is empty"
+# Taskcluster "export-<src>-<trg>" task ID
+[[ -z "${TASK_ID}" ]] && echo "TASK_ID is empty"
 
-SRC=ru
-TRG=en
-EXPERIMENT=all_mtdata_and_opus2
-ROOT_DIR="bergamot-training/models"
-
-SNAKEPIT_DIR="${ROOT_DIR}/${SRC}-${TRG}/${EXPERIMENT}/exported"
 LOCAL_DIR="../models/dev/${SRC}${TRG}"
+TC_PREFIX="https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task"
 
 rm -rf "${LOCAL_DIR}"
 mkdir "${LOCAL_DIR}"
 
-pit pull home "${SNAKEPIT_DIR}/lex.50.50.${SRC}${TRG}.s2t.bin.gz" "${LOCAL_DIR}"
-pit pull home "${SNAKEPIT_DIR}/model.${SRC}${TRG}.intgemm.alphas.bin.gz" "${LOCAL_DIR}"
-pit pull home "${SNAKEPIT_DIR}/vocab.${SRC}${TRG}.spm.gz" "${LOCAL_DIR}"
+wget --directory-prefix="${LOCAL_DIR}" \
+  ${TC_PREFIX}/${TASK_ID}/runs/0/artifacts/public%2Fbuild%2Flex.50.50.${SRC}${TRG}.s2t.bin.gz
+wget --directory-prefix="${LOCAL_DIR}" \
+  ${TC_PREFIX}/${TASK_ID}/runs/0/artifacts/public%2Fbuild%2Fmodel.${SRC}${TRG}.intgemm.alphas.bin.gz
+wget --directory-prefix="${LOCAL_DIR}" \
+ ${TC_PREFIX}/${TASK_ID}/runs/0/artifacts/public%2Fbuild%2Fvocab.${SRC}${TRG}.spm.gz
+
