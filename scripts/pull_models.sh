1a852060146bcf5090caa785c50faee7b59e2dc9
Evgeny Pavlov <epavlov@mozilla.com>
2024-04-03 10:25:17 -0700

    Update Lithuanian to English with a more robust model (#144)
    * Update the model downloading script to use Taskcluster
    
    * Update Lithuanian to English with a more robust model
    
    * Fix output directory
    
    * Remove Bergamot evals for lt-en
    
    * Update evaluation results [skip ci]
    
    * Update model registry [skip ci]
    
    * Update docs [skip ci]
    
    ---------
    
    Co-authored-by: CircleCI evaluation job <ci-models-evaluation@firefox-translations>

--- scripts/pull_models.sh
+++ scripts/pull_models.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+#
+# Downloads exported models from Taskcluster artifacts
+#
+# Example:  SRC=lt TRG=en TASK_ID=SjPZGW9CRYeb9PQr68jCUw bash pull_models.sh
+#
 
-# Downloads models from snakepit cluster
+[[ -z "${SRC}" ]] && echo "SRC is empty"
+[[ -z "${TRG}" ]] && echo "TRG is empty"
+# Taskcluster "export-<src>-<trg>" task ID
+[[ -z "${TASK_ID}" ]] && echo "TASK_ID is empty"
 
-SRC=ru
-TRG=en
-EXPERIMENT=all_mtdata_and_opus2
-ROOT_DIR="bergamot-training/models"
-
-SNAKEPIT_DIR="${ROOT_DIR}/${SRC}-${TRG}/${EXPERIMENT}/exported"
 LOCAL_DIR="../models/dev/${SRC}${TRG}"
+TC_PREFIX="https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task"
 
 rm -rf "${LOCAL_DIR}"
 mkdir "${LOCAL_DIR}"
 
-pit pull home "${SNAKEPIT_DIR}/lex.50.50.${SRC}${TRG}.s2t.bin.gz" "${LOCAL_DIR}"
-pit pull home "${SNAKEPIT_DIR}/model.${SRC}${TRG}.intgemm.alphas.bin.gz" "${LOCAL_DIR}"
-pit pull home "${SNAKEPIT_DIR}/vocab.${SRC}${TRG}.spm.gz" "${LOCAL_DIR}"
+wget -O "${LOCAL_DIR}"/lex.50.50.${SRC}${TRG}.s2t.bin.gz \
+  ${TC_PREFIX}/${TASK_ID}/runs/0/artifacts/public%2Fbuild%2Flex.50.50.${SRC}${TRG}.s2t.bin.gz
+wget -O "${LOCAL_DIR}"/model.${SRC}${TRG}.intgemm.alphas.bin.gz \
+  ${TC_PREFIX}/${TASK_ID}/runs/0/artifacts/public%2Fbuild%2Fmodel.${SRC}${TRG}.intgemm.alphas.bin.gz
+wget -O "${LOCAL_DIR}"/vocab.${SRC}${TRG}.spm.gz \
+ ${TC_PREFIX}/${TASK_ID}/runs/0/artifacts/public%2Fbuild%2Fvocab.${SRC}${TRG}.spm.gz
+
+git add "${LOCAL_DIR}"/*
+
