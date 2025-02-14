9804b89f686dc53db64dbe8d40e04974c18b6bd0
evgeny pavlov <epavlov@mozilla.com>
2025-02-13 18:48:03 -0800

    Don't run everything

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-python3 eval/evaluate.py \
-  --translators=bergamot,microsoft,google,argos,nllb,opusmt \
-  --pairs=all --skip-existing \
-  --results-dir=/models/evaluation --models-dir=/models/models/dev \
-  --gpus=1 --evaluation-engine=comet,bleu
+#python3 eval/evaluate.py \
+#  --translators=bergamot,microsoft,google \
+#  --pairs=all --skip-existing \
+#  --results-dir=/models/evaluation --models-dir=/models/models/dev \
+#  --gpus=1 --evaluation-engine=comet,bleu
 
 python3 eval/evaluate.py \
-  --translators=bergamot,microsoft,google,argos,nllb,opusmt \
+  --translators=bergamot,microsoft,google \
   --pairs=all --skip-existing \
   --results-dir=/models/evaluation --models-dir=/models/models/prod \
   --gpus=1 --evaluation-engine=comet,bleu
