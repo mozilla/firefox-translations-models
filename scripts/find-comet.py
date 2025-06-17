76a2660654b42d6846c96cccf9cdb73fc8dda765
evgeny pavlov <epavlov@mozilla.com>
2025-06-17 15:19:38 -0700

    Remove results for all bergamot models

--- scripts/find-comet.py
+++ scripts/find-comet.py
@@ -6,7 +6,7 @@ signal(SIGPIPE, SIG_DFL)
 
 patterns = ["/".join(["evaluation"] + x.split("/")[1:3]) for x in sys.stdin.readlines()]
 bleu_paths = [
-    g + "\n" for p in patterns for g in glob.glob(p[:-2] + "-" + p[-2:] + "/*.bergamot.*.comet")
+    g + "\n" for p in patterns for g in glob.glob(p[:-2] + "-" + p[-2:] + "/*.bergamot*.comet")
 ]
 
 sys.stdout.writelines(bleu_paths)
