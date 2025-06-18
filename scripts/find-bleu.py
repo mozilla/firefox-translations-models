4019b1f7fd1df39874a6f82579d8cdb1eacd23ac
Evgeny Pavlov <epavlov@mozilla.com>
2025-06-18 11:30:32 -0700

    Add support for multiple model architectures (#232)
    * Group models by architecture
    
    * Remove old dirs
    
    * Rename evals to use bergamot arch
    
    * Update reports to use multiple archs
    
    * Update readme
    
    * Remove deploy step
    
    * Update scripts
    
    * Reformat
    
    * Add enhu tiny
    
    * Force rerun evals
    
    * Update evaluation results [skip ci]
    
    * Rollback force CI
    
    * Remove results for all bergamot models
    
    * Remove model registry and update readme
    
    * Multiple comet by 100
    
    * Do not multiply by 100 results in .json
    
    ---------
    
    Co-authored-by: CircleCI evaluation job <ci-models-evaluation@firefox-translations>

--- scripts/find-bleu.py
+++ scripts/find-bleu.py
@@ -6,7 +6,7 @@ signal(SIGPIPE, SIG_DFL)
 
 patterns = ["/".join(["evaluation"] + x.split("/")[1:3]) for x in sys.stdin.readlines()]
 bleu_paths = [
-    g + "\n" for p in patterns for g in glob.glob(p[:-2] + "-" + p[-2:] + "/*.bergamot.*.bleu")
+    g + "\n" for p in patterns for g in glob.glob(p[:-2] + "-" + p[-2:] + "/*.bergamot*.bleu")
 ]
 
 sys.stdout.writelines(bleu_paths)
