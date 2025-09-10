65ed1c130a7c15f6d4580ed548a3fc9295823e3e
Evgeny Pavlov <epavlov@mozilla.com>
2025-09-10 15:36:55 -0700

    Revert "Disable other translators"
    This reverts commit 6e138f72371da64cef5f1494b1fe7c9ff38de6ba.

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -3,20 +3,20 @@
 set -e
 
 python3 eval/evaluate.py \
-  --translators=bergamot,microsoft,google \
+  --translators=bergamot,microsoft,google,argos,nllb,opusmt \
   --pairs=all --skip-existing \
   --results-dir=/models/evaluation --models-dir=/models/models/tiny \
   --gpus=1 --evaluation-engine=comet,bleu
 
 python3 eval/evaluate.py \
-  --translators=bergamot,microsoft,google \
+  --translators=bergamot,microsoft,google,argos,nllb,opusmt \
   --pairs=all --skip-existing \
   --results-dir=/models/evaluation --models-dir=/models/models/base \
   --gpus=1 --evaluation-engine=comet,bleu
 
 
 python3 eval/evaluate.py \
-  --translators=bergamot,microsoft,google \
+  --translators=bergamot,microsoft,google,argos,nllb,opusmt \
   --pairs=all --skip-existing \
   --results-dir=/models/evaluation --models-dir=/models/models/base-memory \
   --gpus=1 --evaluation-engine=comet,bleu
